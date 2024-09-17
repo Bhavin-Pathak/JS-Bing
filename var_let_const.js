55a2c8eef3605e4eb7a5d91931dac3bf54e97c72
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-09-17 11:00:00 +0530

    chore: var-let-const EG..

---
+++ var_let_const.js
@@ -0,0 +1,16 @@
+console.log("Well-its 1 Day in JS");
+var a = "Hello";
+let b = 5;
+const c = "World!!";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+a = 10;
+b = "Bye";
+// c = "Goodbye";  // Error: 'c' is a constant
+
+console.log(a); //10
+console.log(b); // Bye
+console.log(c); // World
