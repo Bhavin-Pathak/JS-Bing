2db475b715638455db935154b33725186abfdf33
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-09-19 12:38:29 +0530

    if-else-loops

---
+++ if-else.js
@@ -0,0 +1,7 @@
+var a = 150;
+var b = 100;
+if (a < b) {
+  console.log("a is less than b");
+} else if (b > a) {
+  console.log("b is Greter than a");
+}
---
+++ loop.js
@@ -0,0 +1,7 @@
+// for (initialization; condition; increment / decrement) {
+//   // code to be executed
+// }
+
+for (let i = 0; i < 5; i++) {
+  console.log(i); // 0, 1, 2, 3, 4
+}
